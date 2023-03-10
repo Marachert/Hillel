9ddc667e9e9de4b881ed9150008bcaa10b011c51
Sergiy <marachert@gmail.com>
2023-03-10 22:38:33 +0100

    Create Count class

---
+++ Лекція 1. GIT/Math/Count.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Math
+{
+    public class Count
+    {
+        internal int SumOfNumbers(int from, int to)
+        {
+            if (to == from)
+                return to;
+
+            return to + SumOfNumbers(from, to - 1);
+
+        }
+    }
+}
---
+++ Лекція 1. GIT/Math/Program.cs
@@ -0,0 +1,5 @@
+﻿using Math;
+
+Count count = new Count();
+
+Console.WriteLine(count.SumOfNumbers(1, 4));
